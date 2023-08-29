80f2208e6b8c2805e245cfac83a5cf981b116803
“Je4nneChe” <chezhanna@mail.ru>
2023-08-30 02:06:36 +0700

    Файл lvl_2/task_2.1.py изменен, добавлено решение

--- lvl_2/task_2.1.py
+++ lvl_2/task_2.1.py
@@ -10,41 +10,53 @@
 # * [5]                             -> min = 5, max = 5
 # функции sorted, max и min использовать нельзя!
 
-def minimum(arr):
-    pass
+# def minimum(arr):
+#     pass
 
-def maximum(arr):
-    pass
+# def maximum(arr):
+#     pass
+
+# Решение с помощью сортировки пузырьком через for:
+
+arr = [[4,6,2,1,9,63,-134,566],[-52, 56, 30, 29, -54, 0, -110],[42, 54, 65, 87, 0],[5]]
+def bubble_for(lst):
+  n = len(lst)
+  for i in range(n - 1):
+    for j in range (n - i - 1):
+      if lst[j] > lst[j + 1]:
+        lst[j],lst[j+1] = lst[j+1],lst[j]
+  return lst
+for lst in arr:
+  print ('*', lst,'-> min =',bubble_for(lst)[0],', max =',bubble_for(lst)[-1] )
+
+# # Решение для одного списка:
+# # Вариант 1
+# arr =  [22, 44, 77, 33, 55, 8, 99]
+# def maximum(arr):
+#     max_num = arr[0]
+#     for num in arr:
+#         if num > max_num:
+#             max_num = num
+#     return max_num
+# def minimum(arr):
+#     min_num = arr[0]
+#     for num in arr:
+#         if num < min_num:
+#             min_num = num
+#     return min_num
+# print("max =", maximum(arr), ", min =", minimum(arr))
 
 
-# Решение
-# Вариант 1
-arr =  [22, 44, 77, 33, 55, 8, 99]
-def maximum(arr):
-    max_num = arr[0]
-    for num in arr:
-        if num > max_num:
-            max_num = num
-    return max_num
-def minimum(arr):
-    min_num = arr[0]
-    for num in arr:
-        if num < min_num:
-            min_num = num
-    return min_num
-print("max =", maximum(arr), ", min =", minimum(arr))
+# # Вариант 2
+# # Инициализация переменных max_number и min_number
+# # Цикл for для прохода по всем элементам списка
 
-
-# Вариант 2
-# Инициализация переменных max_number и min_number
-# Цикл for для прохода по всем элементам списка
-
-numbers = [22, 44, 77, 33, 55, 8, 99]
-max_number, min_number = numbers[0], numbers[0]
-for number in numbers:
-    if number > max_number:
-        max_number = number 
-    if number < min_number:
-        min_number = number
+# numbers = [22, 44, 77, 33, 55, 8, 99]
+# max_number, min_number = numbers[0], numbers[0]
+# for number in numbers:
+#     if number > max_number:
+#         max_number = number 
+#     if number < min_number:
+#         min_number = number
      
-print('max =', max_number, ',', 'min =', min_number)
+# print('max =', max_number, ',', 'min =', min_number)
